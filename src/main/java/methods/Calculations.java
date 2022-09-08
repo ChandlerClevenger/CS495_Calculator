834dc5d42596605e82523f1de23619500b504348
Robert <ibsonnumber1@gmail.com>
2022-09-08 15:48:48 -0400

    Initial commit

---
+++ src/main/java/methods/Calculations.java
@@ -0,0 +1,28 @@
+package methods;
+
+public class Calculations {
+
+    public int add(int left, int right){
+        return left + right;
+    }
+
+    public int subtract(int left, int right){
+        return left - right;
+    }
+
+    public int multiply(int left, int right){
+        return left * right;
+    }
+
+    public int divide(int left, int right){
+        return left / right;
+    }
+
+    public int squareRoot(int number){
+        return (int) Math.sqrt(number);
+    }
+
+    public int square(int number){
+        return number^2;
+    }
+}
